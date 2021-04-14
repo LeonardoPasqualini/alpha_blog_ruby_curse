f28e33cce70815a03e21c84d9164721d7ebaf01d
LeonardoPasqualini <lpleopasqualini@gmail.com>
2021-04-14 11:15:02 +0100

    Classes 138 and 140 - Add sign up feature, users create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy] - 'only' expose only a few of those options
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
