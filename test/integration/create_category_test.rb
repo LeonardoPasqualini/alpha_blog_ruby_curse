9d696e1615e9bcd2ceb6072a969fad50dd95771b
LeonardoPasqualini <lpleopasqualini@gmail.com>
2021-04-19 14:09:29 +0100

    Classes 192 and 194 - Add feture with admin requirement and test (NOT WORKING) and Updated navigation

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example,com", password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
+
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_difference 'Category.count', 1 do
@@ -17,7 +25,7 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_no_difference 'Category.count' do
       post categories_path, params: { category: { name: " " } }
     end
-    assert_match "errors", response.body
+    assert_match "error", response.body
     assert_select 'div.alert'
     assert_select 'h4.alert-heading'
   end
