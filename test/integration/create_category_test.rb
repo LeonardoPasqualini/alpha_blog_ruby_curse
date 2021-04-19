3facdb65a68403c9ccdcce47c6525c9a2be641c0
LeonardoPasqualini <lpleopasqualini@gmail.com>
2021-04-19 11:12:44 +0100

    Classes 188 and 190 - add Integration test for invalid category and Feature listing categories

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -11,4 +11,14 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+  test "get new category form and reject inavalid category submission" do
+    get "/categories/new"
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
 end
